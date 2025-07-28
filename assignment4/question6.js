a7a22a592c11e486e9331c3cb4a97f52cf473f5c
jamieseah <jamseah@gmail.com>
2025-07-28 14:24:33 +0800

    Assignment 4
    Submission for Assignment 4

---
+++ assignment4/question6.js
@@ -0,0 +1,14 @@
+// Modify the code below:
+
+const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
+
+// let data = ()
+// for price in data:
+// 	if price < 3:
+//     data.push(price)
+
+const sortedPrices = [...amdPrices].sort((a, b) => a - b);
+
+const lowestThree = sortedPrices.slice(0, 3);
+
+console.log("The three lowest prices are", lowestThree);
